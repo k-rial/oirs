84616a687e3206c46071b57d62d43de120cab1cb
Kirill <kirill.otdelnov@gmail.com>
2024-05-29 19:11:50 +0300

    fix main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from telebot.service_utils import Image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, Input
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-
-import numpy as np
 import telebot
 
 import os
